c83c12b4803808b6882eda097f21e91cbfd46f88
Rody Molenaar <rody.molenaar@xtrada.nl>
2015-10-12 15:37:42 +0200

    Add compress task

--- gruntfile.js
+++ gruntfile.js
@@ -16,9 +16,23 @@ module.exports = function(grunt) {
                      },
                      files: {"dist/css/mc-portal.css": "less/mc-portal.less"}
                  }
+             },
+             compress: {
+              main: {
+                options: {
+                  archive: 'MC-Portal.zip'
+                },
+                files: [
+                  {src: ['dist/**'], dest: 'MC-Portal/'}, // includes files in path and its subdirs
+                  {src: ['index.html'], dest: 'MC-Portal/'},
+                  {src: ['README.md'], dest: 'MC-Portal/'},
+                  {src: ['LICENSE'], dest: 'MC-Portal/'}
+                ]
+              }
              }
          });
          grunt.loadNpmTasks('grunt-contrib-less');
          grunt.loadNpmTasks('grunt-contrib-watch');
+         grunt.loadNpmTasks('grunt-contrib-compress');
          grunt.registerTask('default', ['watch']);
      };
